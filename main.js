f8207509aec1cc0e0dc8f005e96ae53d5f466f07
yekaos <kyarribas@gmail.com>
2024-03-13 11:06:57 +0100

    adicion cv y nuevo boton

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function imprimir() {
   window.print();
 }
+document.getElementById("pdf").addEventListener("click", function() {
+    var link = document.createElement('a');
+    link.href = 'curriculum_kyllian_arribas.pdf';
+    link.download = 'CV_Kyllian_Arribas.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
